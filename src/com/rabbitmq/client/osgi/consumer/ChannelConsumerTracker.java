024e99c1f004caf9009cc2e28646522473c103e8
Neil Bartlett <njbartlett@gmail.com>
2009-01-05 11:26:43 +0000

    Added notes on OSGifying the RabbitMQ Client Library JAR

--- src/com/rabbitmq/client/osgi/consumer/ChannelConsumerTracker.java
+++ src/com/rabbitmq/client/osgi/consumer/ChannelConsumerTracker.java
@@ -49,3 +49,22 @@ public class ChannelConsumerTracker extends ServiceTracker {
 		context.ungetService(reference);
 	}
 }
+
+class Pair<A, B> {
+	private final A fst;
+	private final B snd;
+
+	public Pair(A fst, B snd) {
+		this.fst = fst;
+		this.snd = snd;
+	}
+
+	public A getFst() {
+		return fst;
+	}
+
+	public B getSnd() {
+		return snd;
+	}
+
+}
